711938eddc689d299827327d00d24931704b9398
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-28 14:38:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let timer = document.querySelector('#timer');
+let currDate = new Date();
+setInterval(()=>{
+	currDate = new Date;
+	 timer.innerHTML = currDate.toLocaleString();
+}, 1000)
